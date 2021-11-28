7a2135ddb9a66b90ebe4a3f143f1c6a8f77d2bab
gimzi <foge1352@gmail.com>
2021-11-28 11:09:02 +0900

    gimzi

---
+++ WebPrac/src/com/company/java/Person.java
@@ -0,0 +1,16 @@
+package com.company.java;
+
+public class Person {
+
+	public static void main(String[] args) {
+		
+		pay();
+
+	}
+   
+	
+	static void pay() {
+		
+		System.out.println( 1+1);
+	}
+}
