e452e235c1582ff5de8298905720e928a4249966
gimzi <foge1352@gmail.com>
2021-11-29 10:34:29 +0900

    211129

--- WebPrac/src/com/company/java/Person.java
+++ WebPrac/src/com/company/java/Person.java
@@ -5,6 +5,7 @@ public class Person {
 	public static void main(String[] args) {
 		
 		pay();
+		dia();
 
 	}
    
@@ -12,5 +13,30 @@ public class Person {
 	static void pay() {
 		
 		System.out.println( 1+1);
+	}
+	
+	static void dia() {
+		
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=5-i; j++) {
+				System.out.print(" ");
+			}
+			for(int j=1; j<=2*i-1; j++) {
+			System.out.print("*");
+				
+			}
+			
+			System.out.println();
+		}
+	    for(int i=4; i>0; i--) {
+	    	for(int j=4;j>=i; j--) {
+	    		System.out.print(" ");
+	    	}
+	    	for(int j=1; j<=2*i-1; j++) {
+    		System.out.print("*");
+    	}
+	    	
+	    	System.out.println();
+	    }
 	}
 }
